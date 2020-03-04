53c5500d494670ed174cdfad53545487be4a51a5
Silke Jandl <silke8421@gmx.at>
2020-03-04 11:31:56 +0100

    logische operatoren

--- src/edu/campus02/Main.java
+++ src/edu/campus02/Main.java
@@ -24,5 +24,27 @@ public class Main {
 
         boolean result = esVerwirrtMich || ichKenneMichAus;
         System.out.println("Es verwirrt mich ODER ich kenne mich aus: " + result);
+
+        //Beispiel 3 PP (1 > 3 UND NICHT 3-1 < 5) ODER 2==4*4
+        //              (1 > 3 && ! 3 - 1 < 5) || 2 == 4*4
+
+        boolean teil1;
+        boolean teil2;
+        boolean ergebnis3;
+
+        teil2 = 2 == 4*4;
+        teil1 = 1 > 3 && ! (3 - 1 < 5);
+
+        ergebnis3 = teil1 || teil2;
+
+        System.out.println("Ergebnis 3: " + ergebnis3);
+
+        // Bsp 4 PP (6 >= 2 * 3 || 2 - 3 < 0) && (1 * 1 == 1 && 1 != 1+1)
+
+        boolean part1 = 6 >= 2 * 3 || 2 - 3 < 0;
+        boolean part2 = 1 * 1 == 1 && 1 != 1+1;
+        boolean ergebnis4 = part1 && part2;
+
+        System.out.println("Ergebnis 4: " + ergebnis4);
     }
 }
